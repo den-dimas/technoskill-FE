c42e78c12fdbd58a8f748c2eedb48555070d4e69
Dimas <dimass.drm@gmail.com>
2024-07-09 19:46:33 +0700

    Version@0.0.0

---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div
+      id="homepage"
+      className="w-screen h-screen flex items-center justify-center"
+    >
+      <h1>Home Page</h1>
+    </div>
+  );
+}
+
+export default Home;
