6c47868c7c25e436f8759fdfe972d24e2588af98
Dimas <dimass.drm@gmail.com>
2024-07-09 19:49:00 +0700

    Add Tailwind color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-blue": "#304463",
+        "light-blue": "#7D8ABC",
+        pink: "#FFC7ED",
+        yellow: "#FFF8DB",
+      },
+    },
   },
   plugins: [],
 };
